a82d4002cbbffd5c1d96b41af52013c35870f2ff
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-23 01:09:09 +0500

    body parses added

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const mongoose = require("mongoose");  
+const mongoose = require("mongoose");
 
 const app = express();
+
+//DB Config
+const db = require("./config/keys").MONGO_URI;
+
+// Connect to mongo
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongo DB Connected Successfully"))
+  .catch((err) => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+// BodyParser
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 app.use("/", require("./Routes/index"));
@@ -15,4 +30,3 @@ app.use("/users", require("./Routes/user"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
-// https://youtu.be/6FOq4cUdH8k?list=LLB3AQLFERVUvgJfbCz5mNww&t=1319
