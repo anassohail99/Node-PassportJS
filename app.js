f6de661218fc362514b8b25f3ce46eff32c70ed9
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-24 01:32:38 +0500

    passport and sessions added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+// Passport Config
+require("./config/passport")(passport);
 
 //DB Config
 const db = require("./config/keys").MONGO_URI;
@@ -23,6 +29,30 @@ app.set("view engine", "ejs");
 // BodyParser
 app.use(express.urlencoded({ extended: false }));
 
+// Express Session MiddleWare
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Middle ware for passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 // ROUTES
 app.use("/", require("./Routes/index"));
 app.use("/users", require("./Routes/user"));
@@ -30,3 +60,4 @@ app.use("/users", require("./Routes/user"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+// https://youtu.be/6FOq4cUdH8k?list=LLB3AQLFERVUvgJfbCz5mNww&t=2467
