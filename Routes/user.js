a2f09e44dc1d2f451065ab8a9ff3e8a41e8ab19e
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-23 23:20:08 +0500

    Variable name change

--- Routes/user.js
+++ Routes/user.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+// user model
+const User = require("../models/User");
 
 // login page
 router.get("/login", (req, res) => {
@@ -13,21 +17,21 @@ router.get("/register", (req, res) => {
 
 // Register user
 router.post("/register", (req, res) => {
-  const { name, email, password, confirmPassword } = req.body;
+  const { name, email, password, password2 } = req.body;
   let errors = [];
 
   // check require fields
-  if (!name || !email || !password || !confirmPassword) {
+  if (!name || !email || !password || !password2) {
     errors.push({ msg: "Please fill in all fields" });
   }
 
   // check passwords
-  if (password !== confirmPassword) {
+  if (password != password2) {
     errors.push({ msg: "Passwords donot matches" });
   }
 
   // Checks password lengths
-  if (password.length <= 6) {
+  if (password.length < 6) {
     errors.push({ msg: "Password should be atleast greater than 6" });
   }
 
@@ -38,12 +42,46 @@ router.post("/register", (req, res) => {
       name,
       email,
       password,
-      confirmPassword,
+      password2,
     });
   } else {
-    res.send("pass");
+    // validation passed
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        // check if user exists
+        errors.push({ msg: "Email is already registered" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+
+        //Hashing password
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            // set password to hased one
+            newUser.password = hash;
+            // Save user
+            newUser
+              .save()
+              .then(() => {
+                res.redirect("/users/login");
+              })
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    });
   }
 });
 
 module.exports = router;
-// https://youtu.be/6FOq4cUdH8k?list=LLB3AQLFERVUvgJfbCz5mNww&t=1295
